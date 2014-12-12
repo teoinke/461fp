829d16e2ef170f8dd4f243c93d7e302641d83684
Gustavo Mendonca <guzzgm@gmail.com>
2014-12-12 17:18:29 -0600

    Javascript Folder

---
+++ js/header-menu.js
@@ -0,0 +1,8 @@
+//home
+$("#goToContact").click(function() {
+    $("#main-content").load("home.html");
+});
+//contact
+$("#goToContact").click(function() {
+    $("#main-content").load("contact.html");
+});
---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+//header
+$(function(){
+    $("#header-page").load("header.html");
+});
+//footer
+$(function(){
+    $("footer").load("footer.html");
+});
+//about - main page
+$(function(){
+    $("#main-content").load("about.html");
+});
